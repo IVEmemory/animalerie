36802320c8700a101427727dfebf80c84fb2e7cc
Jessy <jessy@hotmail.com>
2020-05-19 13:58:48 -0400

    Dépot initial

---
+++ Vue/Apropos/index.php
@@ -0,0 +1,26 @@
+<?php $titre = 'Le Blogue du prof version 1.0.0.1'; ?>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>A propos de la page web</title>
+    </head>
+    <body>
+
+    <p> <strong>Créateur</strong> : Jessy Bérubé <br>
+        <strong>Le titre du cours</strong>: 420-4A4 MO Web et Bases de données.
+        <br>Hiver 2020, Collège Montmorency
+    </p>
+    <img src="images/DatabasePicture.png" alt="" style="width:350px;height:300px;" >
+    <img src="images/dataBaseOriginal.png" alt="" style="width:350px;height:300px;" >
+    <a href="http://www.databaseanswers.org/data_models/dog_breeding/index.htm"><br>Liens de la BD original</a>
+
+    <form action="index.php" method="post">
+			
+            <input type="submit" name="retour" value="Retour" />
+           
+    </form>
+    
+
+    </body>
+</html>
